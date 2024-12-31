a1af568e1ba1f8139fd6646c604e32f8630f3310
Franco Salas <franco_10_65@hotmail.com>
2024-12-31 13:54:55 -0300

    modif api

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -12,7 +12,7 @@ public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=f1f7864f";
+    private final String API_KEY = "&apikey=" + System.getenv("apideOMDB");
     private ConvierteDatos conversor = new ConvierteDatos();
     private List<DatosSerie> datosSeries = new ArrayList<>();
     private SerieRepository repositorio;
